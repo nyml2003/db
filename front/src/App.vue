5ab614c596c458fc0c46f0050af88d299052c654
nyml <2492181618@qq.com>
2023-05-18 06:42:38 +0800

    彻底可复用

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,173 +1,40 @@
 <template>
+ 
   <el-container>
     <el-aside style="width: fit-content">
-      <!-- <el-menu :router="true" class="el-menu-vertical-demo" default-active="2">
-    <el-menu-item index="/">
-      <el-icon><HomeFilled /></el-icon>
-      首页
-    </el-menu-item>
-    <el-sub-menu>
-      <template #title><el-icon><Document /></el-icon>原始表格</template>
-    <el-menu-item index="/patient">
-      <el-icon><Postcard /></el-icon>
-      患者
-    </el-menu-item>
-    <el-menu-item index="/dept">
-      <el-icon><Postcard /></el-icon>
-      部门
-    </el-menu-item>
-    <el-sub-menu index="">
-      <template #title><el-icon><Postcard /></el-icon>员工</template>
-      <el-menu-item index="/doctor">
-        <el-icon><Postcard /></el-icon>医生
-      </el-menu-item>
-    </el-sub-menu> 
-    <el-menu-item index="/Title">
-      <el-icon><Postcard /></el-icon>
-      职称
-    </el-menu-item>
-    <el-menu-item index="/Salary">
-      <el-icon><Postcard /></el-icon>
-      工资
-    </el-menu-item>
-    <el-menu-item index="/GoDown_Entry">
-      <el-icon><Postcard /></el-icon>
-      入库主单
-    </el-menu-item>
-    <el-menu-item index="/GoDown_Slave">
-      <el-icon><Postcard /></el-icon>
-      入库从单
-    </el-menu-item>
-    <el-menu-item index="/Medicine">
-      <el-icon><Postcard /></el-icon>
-      药品
-    </el-menu-item>
-    <el-menu-item index="/Diagnosis">
-      <el-icon><Postcard /></el-icon>
-      就诊
-    </el-menu-item>
-    
-
-    </el-sub-menu>
-     -->
-      <!-- </el-menu> -->
-      <el-menu router :default-active="$route.path" class="el-menu-vertical-demo">
+      <el-menu
+        style="width: fit-content"
+        class="el-menu-demo flex"
+        router
+        :default-active="$route.path"
+      >
         <el-menu-item index="/">
           <el-icon>
             <HomeFilled />
           </el-icon>
           首页
+        </el-menu-item>     
+        <el-menu-item
+          v-for="(item,id) in items"
+          :key="id"
+          :index="`1:${id}`"
+          :route="{ name:'data', query: { item:JSON.stringify({dataColumns:item.dataColumns,table:item.table,pk:item.pk,dateCol:item.dateCol,datetimeCol:item.datetimeCol}) } }"
+        >
+          <el-icon><HomeFilled /></el-icon>{{item.name}}
         </el-menu-item>
-        <el-sub-menu index="">
-          <template #title><el-icon>
-              <Document />
-            </el-icon>原始表格</template>
-          <el-menu-item v-for="item in items" :key="item.id" index="data_raw"
-            :route="{ path: item.url, params: {} }">
-            <template #title><el-icon>
-                <Postcard />
-              </el-icon>{{ item.name }}</template>
-          </el-menu-item>
-
-        </el-sub-menu>
       </el-menu>
     </el-aside>
     <el-main>
-      <router-view />
+     
+      <router-view :key=this.$route.fullPath></router-view>
     </el-main>
-
   </el-container>
 </template>
 
 <script setup>
-const items = [
-  {
-    id: 1, url: "/patient", name: "患者",
-    dataColumns: [
-      "Pno",
-      "Pname",
-      "Pid",
-      "Pino",
-      "Pmno",
-      "Psex",
-      "Pbd",
-      "Padd"
-    ], table: "`cs2305.patient`", pk: "Pno", dateCol: "Pbd"
-  },
-  {
-    id: 2, url: "/dept", name: "部门", dataColumns: [
-      "DeptNo",
-      "DeptName",
-      "ParentDeptNo",
-      "Manager"
-    ], table: "`cs2305.dept`", pk: "DeptNo", dateCol: null
-  },
-  {
-    id: 3, url: "/doctor", name: "医生", dataColumns: [
-      "Dno",
-      "Dname",
-      "Dsex",
-      "Dage",
-      "Ddeptno",
-      "Tno"
-    ], table: "`cs2305.doctor`", pk: "Dno", dateCol: null
-  },
-  {
-    id: 4, url: "/Title", name: "职称", dataColumns: [
-      "Tno",
-      "Sno",
-      "Ttype",
-      "Ttrade"
-    ], table: "`cs2305.title`", pk: "Tno", dateCol: null
-  },
-  {
-    id: 5, url: "/Salary", name: "工资", dataColumns: [
-      "Sno",
-      "Slevel",
-      "Snumber"
-    ], table: "`cs2305.Salary`", pk: "Sno", dateCol: null
-  },
-  {
-    id: 6, url: "/GoDown_Entry", name: "入库主单", dataColumns: [
-      "GMno",
-      "GMdate",
-      "GMname"
-    ], table: "`cs2305.godown_entry`", pk: "GMno", dateCol: null
-  },
-  {
-    id: 7, url: "/GoDown_Slave", name: "入库从单", dataColumns: [
-      "GSno",
-      "GMno",
-      "Mno",
-      "GSnumber",
-      "GSunit",
-      "GSbatch",
-      "GSprice",
-      "GSexpdate"
-    ], table: "`cs2305.godown_slave`", pk: "GSno", dateCol: null
-  },
-  {
-    id: 8, url: "/Medicine", name: "药品", dataColumns: [
-      "Mno",
-      "Mname",
-      "Munit",
-      "Mprice",
-      "Mexpdate"
-    ], table: "`cs2305.medicine`", pk: "Mno", dateCol: null
-  },
-  {
-    id: 9, url: "/Diagnosis", name: "就诊",dataColumns: [
-      "DGno",
-      "Pno",
-      "Dno",
-      "Symptom",
-      "Diagnosis",
-      "DGtime",
-      "Rfee"
-    ], table: "`cs2305.diagnosis`", pk: "DGno", dateCol: null
-  },
-  { id: 10, url: "/Recipe_Master", name: "处方主单" },
-]
+import {useStore} from 'vuex'
+import { ref } from 'vue';
+const items=ref(useStore().state.rawTableParanms)
 </script>
 
 <style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,14 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Patient from '../views/PatientRaw.vue'
-import Dept from '../views/DeptRaw.vue'
-import Doctor from '../views/DoctorRaw.vue'
-import Title from '../views/TitleRaw.vue'
-import Salary from '../views/SalaryRaw.vue'
-import GoDown_Entry from '../views/Godown_Entry_Raw.vue'
-import GoDown_Slave from '../views/Godown_Slave_Raw.vue'
-import Medicine from '../views/MedicineRaw.vue'
-import Diagnosis from '../views/DiagnosisRaw.vue'
 const routes = [
   {
     path: '/',
@@ -16,53 +7,8 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/patient',
-    name: 'patient',
-    component: Patient
-  },
-  {
-    path: '/dept',
-    name: 'dept',
-    component: Dept
-  },
-  {
-    path: '/doctor',
-    name: 'doctor',
-    component: Doctor
-  },
-  {
-    path: '/title',
-    name: 'title',
-    component: Title
-  },
-  {
-    path: '/salary', 
-    name: 'salary',
-    component: Salary
-  },
-  {
-    path: '/godown_entry',
-    name: 'godown_entry',
-    component: GoDown_Entry
-  },
-  {
-    path: '/godown_slave',
-    name: 'godown_slave',
-    component: GoDown_Slave
-  },
-  {
-    path: '/medicine',
-    name: 'medicine',
-    component: Medicine
-  },
-  {
-    path: '/diagnosis',
-    name: 'diagnosis',
-    component: Diagnosis
-  },
-  {
-    path:'/data_raw/:table',
-    name:'data_raw',
+    path:'/data',
+    name:'data',
     component:()=>import('../views/DataRaw.vue')
   }
 ]
